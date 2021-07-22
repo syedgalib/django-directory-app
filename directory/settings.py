8902dce8dd54aa77dbabe7489eba7d680ddc3345
Syed Galib Ahmed <syedgalibahmed@gmail.com>
2021-07-22 23:09:20 +0600

    enabled api app

--- directory/settings.py
+++ directory/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'listings.apps.ListingsConfig',
-    # 'api.apps.ApiConfig'
+    'api.apps.ApiConfig'
 ]
 
 MIDDLEWARE = [
