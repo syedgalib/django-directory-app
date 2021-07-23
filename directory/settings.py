28b08267119e3a3ef84459238ab89eb7116d2071
Syed Galib Ahmed <syedgalibahmed@gmail.com>
2021-07-23 15:59:05 +0600

    added static assets

--- directory/settings.py
+++ directory/settings.py
@@ -131,3 +131,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "directory/static",
+]
